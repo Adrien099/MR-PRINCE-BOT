00b39ebbe9fd88916ec531754ced9925c291d579
Adrien099 <arbe0243@gmail.com>
2024-05-24 14:04:20 +0530

    Update main-ggp.js

--- plugins/main-ggp.js
+++ plugins/main-ggp.js
@@ -1,14 +1,14 @@
 let handler = async m => m.reply(`
 ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆
-≡★☆🅟🅡🅘🅝🅒🅔 🅣🅗🅔 🅞🅦🅝🅔🅡☆★≡
+≡★☆Bear☆★≡
 
-★𝚂𝚄𝙿𝙿𝙾®𝚃 𝙶𝚁𝙾𝚄𝙿 𝙻𝙸𝙽𝙺★
+★𝙶𝚁𝙾𝚄𝙿 𝙻𝙸𝙽𝙺★
 ─────────────
 ▢ ★☆☆🛡️𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋🛡️☆☆★
 
-🅛🅘🅝🅚: https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP
+🅛🅘🅝🅚: https://chat.whatsapp.com/K6j6xmYolmqEEos38OUKmk
 
-🅛🅘🅝🅚: https://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11
+🅛🅘🅝🅚: https://chat.whatsapp.com/K6j6xmYolmqEEos38OUKmk
 
 ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★
 ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆
